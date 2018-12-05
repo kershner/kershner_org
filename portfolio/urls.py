46167a5963ecd33bb414374cb58f7f342b79a27c
Tyler <tylerkershner@gmail.com>
2018-12-04 23:47:12 -0500

    First pass at music page

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('', portfolio_views.home),
+    path('', portfolio_views.home, name='home'),
+    path('music/', portfolio_views.music, name='music'),
     path('admin/', admin.site.urls),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from django.conf import settings
 
 
 def home(request):
     template_vars = {
-        'title': 'shitter'
+        'title': 'Tyler Kershner'
     }
     return render(request, 'home.html', template_vars)
+
+
+def music(request):
+    base_s3_url = 'https://{}/static/music'.format(settings.AWS_S3_CUSTOM_DOMAIN)
+    template_vars = {
+        'title': 'Tyler Kershner - Music',
+        'base_s3_url': base_s3_url
+    }
+    return render(request, 'music.html', template_vars)
