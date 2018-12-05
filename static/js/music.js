4abe8b7e84632a5ac10bd1ae948b0e14d16fbe7b
Tyler <tylerkershner@gmail.com>
2018-12-05 01:12:27 -0500

    Add dumb old video to music page

--- static/js/music.js
+++ static/js/music.js
@@ -1,9 +1,12 @@
 var music = {
-    songContainer : 'amplitude-song-container'
+    songContainer   : 'amplitude-song-container',
+    'oldVideoUrl'   : 'https://www.youtube.com/embed/w1t-Mevr9YM'
 };
 
 music.init = function() {
     Amplitude.init(amplitudeOptions);
+    deferVideoLoad();
+
 
     // Onclick for song containers
     document.querySelectorAll('.' + music.songContainer).forEach(function(e) {
@@ -11,4 +14,11 @@ music.init = function() {
             console.log('clicked!');
         });
     });
+
+    function deferVideoLoad() {
+        var oldVideoIfr = document.getElementById('old-music-video-ifr');
+        setTimeout(function (){
+            oldVideoIfr.src = music.oldVideoUrl
+        }, 1500);
+    }
 };
