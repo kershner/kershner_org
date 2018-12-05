6b1e32ac5e78265cf258f1265dc5c546fc76acc8
Tyler <tylerkershner@gmail.com>
2018-12-05 17:20:56 -0500

    Add loops/songs toggle to player

--- static/js/music.js
+++ static/js/music.js
@@ -1,19 +1,50 @@
 var music = {
     songContainer   : 'amplitude-song-container',
-    'oldVideoUrl'   : 'https://www.youtube.com/embed/w1t-Mevr9YM'
+    oldVideoUrl     : 'https://www.youtube.com/embed/w1t-Mevr9YM'
 };
 
 music.init = function() {
     Amplitude.init(amplitudeOptions);
+    playlistSelect();
     deferVideoLoad();
-
 
     // Onclick for song containers
     document.querySelectorAll('.' + music.songContainer).forEach(function(e) {
         e.addEventListener('click', function() {
-            console.log('clicked!');
+            console.log('clicked on a song!');
         });
     });
+
+    function playlistSelect() {
+        // Onclick for playlist options
+        document.querySelectorAll('.playlist-option').forEach(function(e) {
+            e.addEventListener('click', function(e) {
+                togglePlaylist(e.target);
+            });
+        });
+    }
+
+    function togglePlaylist(element) {
+        var type = element.getAttribute('data-type'),
+            songsContainer = document.getElementById('songs'),
+            loopsContainer = document.getElementById('loops');
+
+        // Remove/add active class
+        document.querySelectorAll('.playlist-option').forEach(function(e) {
+            removeClass(e, 'active');
+        });
+        addClass(element, 'active');
+
+        if (type === 'loops') {
+            // Show loops, hide songs
+            removeClass(loopsContainer, 'hidden');
+            addClass(songsContainer, 'hidden');
+        } else {
+            // Show songs, hide loops
+            removeClass(songsContainer, 'hidden');
+            addClass(loopsContainer, 'hidden');
+        }
+    }
 
     function deferVideoLoad() {
         var oldVideoIfr = document.getElementById('old-music-video-ifr');
