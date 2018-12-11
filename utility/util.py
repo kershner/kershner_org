5770b44b6acce19dbaff5a24442d4f94a99ef57d
Tyler <tylerkershner@gmail.com>
2018-12-11 00:27:03 -0500

    Rename config to site_config to avoid conflict

--- utility/util.py
+++ utility/util.py
@@ -5,7 +5,7 @@ import os
 def get_parameters():
     # Load params from JSON file
     base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    config_path = os.path.join(base_dir) + '/config'
+    config_path = os.path.join(base_dir) + '/site_config'
     with open(config_path + '/parameters.json') as f:
         params = json.load(f)
         return params
