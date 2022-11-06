467a3b5233c816487e911a2e882362a674789fc9
Tyler <tylerkershner@gmail.com>
2022-11-05 23:38:52 -0400

    add prod logging to file, path fixing

--- site_config/settings/prod.py
+++ site_config/settings/prod.py
@@ -9,3 +9,24 @@ ALLOWED_HOSTS = [
 DEBUG = False
 STATIC_URL = 'https://%s/%s/static/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "root": {"level": "INFO", "handlers": ["file"]},
+    "handlers": {
+        "file": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename": "/var/log/django.log",
+            "formatter": "app",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "INFO",
+            "propagate": True
+        },
+    },
+}
