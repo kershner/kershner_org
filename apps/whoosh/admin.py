c8913eb17e2acd0254e00f955060277ae82c0624
Tyler <tylerkershner@gmail.com>
2022-11-14 09:50:36 -0500

    add admin doppelganger filter

--- apps/whoosh/admin.py
+++ apps/whoosh/admin.py
@@ -1,7 +1,29 @@
+from django.contrib.admin import SimpleListFilter
 from django.utils.html import format_html
 from apps.whoosh.models import Whoosh
 from django.contrib import admin
 from django.urls import reverse
+
+
+class DoppelgangerFilter(SimpleListFilter):
+    title = 'Doppelgänger'
+    parameter_name = 'doppel'
+
+    def lookups(self, request, model_admin):
+        return (
+            (True, 'True'),
+            (False, 'False'),
+        )
+
+    def queryset(self, request, queryset):
+        queryset = Whoosh.objects.all()
+
+        if self.value() == 'False':
+            return queryset.filter(doppelganger=None)
+        elif self.value() == 'True':
+            return queryset.filter(doppelganger__isnull=False)
+
+        return queryset
 
 
 @admin.register(Whoosh)
@@ -12,7 +34,7 @@ class WhooshAdmin(admin.ModelAdmin):
     readonly_fields = ['ip', 'doppelganger', 'uniq_id', 'user_agent', 'created', 'processed', 'source_video',
                        'processed_video', 'thumbnail', 'saved_video', 'saved_thumbnail', 'settings_hash',
                        'saved', 'hidden']
-    list_filter = ['saved']
+    list_filter = ['saved', DoppelgangerFilter]
 
     @staticmethod
     def thumbnail_preview(obj):
