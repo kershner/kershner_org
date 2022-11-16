8d21c4c4ad23ff512f6088aba1f1579292a45663
Tyler <tylerkershner@gmail.com>
2022-11-15 20:50:30 -0500

    make technologies section into links

--- apps/project/admin.py
+++ apps/project/admin.py
@@ -1,11 +1,19 @@
+from apps.project.models import Project, ProjectTechnology
 from django.template.loader import render_to_string
-from apps.project.models import Project
 from django.contrib import admin
+
+
+class ProjectTechnologyInline(admin.TabularInline):
+    fields = ['title', 'link']
+    model = ProjectTechnology
+    extra = 1
+    verbose_name = 'Project Technology'
+    verbose_name_plural = 'Project Technologies'
 
 
 
 # Admin config for this model
-# https://docs.djangoproject.com/en/2.0/ref/contrib/admin/#django.contrib.admin.ModelAdmin
+# https://docs.djangoproject.com/en/4.1/ref/contrib/admin/#django.contrib.admin.ModelAdmin
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('title', 'id', 'created_at', 'position', 'change_position')
@@ -14,6 +22,7 @@ class ProjectAdmin(admin.ModelAdmin):
     show_full_result_count = True
     readonly_fields = ['position']
     ordering  = ('position',)
+    inlines = [ProjectTechnologyInline]
 
     @staticmethod
     def change_position(obj):
--- apps/project/models.py
+++ apps/project/models.py
@@ -10,12 +10,21 @@ def rename_upload(instance, filename):
     return os.path.join(path, new_filename)
 
 
+class ProjectTechnology(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    project = models.ForeignKey('project.Project', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, null=False, blank=False)
+    link = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
 class Project(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True, editable=False)
     title = models.CharField(max_length=255)
     blurb = models.TextField()
-    technologies = models.CharField(max_length=255)
     extra_notes = models.TextField(blank=True)
     icon = models.ImageField(upload_to=rename_upload)
     # (actual DB value, human-readable name)
@@ -28,12 +37,26 @@ class Project(models.Model):
     image_2 = models.ImageField(upload_to=rename_upload, blank=True)
     image_3 = models.ImageField(upload_to=rename_upload, blank=True)
     drop_shadow = models.BooleanField(default=False, help_text='Apply drop shadow to images.')
-    site_url = models.CharField(max_length=255)
+    site_url = models.CharField(max_length=255, null=True, blank=True)
     position = models.IntegerField(default=0)
 
     def __init__(self, *args, **kwargs):
         super(Project, self).__init__(*args, **kwargs)
         self.__original_position = self.position
+
+    def __str__(self):
+        return 'ID: %d | %s' % (self.id, self.title)
+
+    @property
+    def project_technologies(self):
+        return ProjectTechnology.objects.filter(project=self).order_by('id').all()
+
+    @property
+    def tech_has_links(self):
+        for tech in self.project_technologies:
+            if tech.link:
+                return True
+        return False
 
     def move_position(self, direction):
         all_projects = list(Project.objects.order_by('position').all())
@@ -55,7 +78,3 @@ class Project(models.Model):
             proj_to_update.append(proj)
 
         Project.objects.bulk_update(proj_to_update, ['position'])
-
-
-    def __str__(self):
-        return 'ID: %d | %s' % (self.id, self.title)
