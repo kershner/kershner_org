bcb2d9254c335b8881d58394b2fa6349e225422a
Tyler <tylerkershner@gmail.com>
2023-04-29 22:46:55 -0400

    updating paths

--- apps/color_doodle/react-frontend/webpack.config.js
+++ apps/color_doodle/react-frontend/webpack.config.js
@@ -1,13 +1,17 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
 const baseDir = "./src/";
 
 //const publicUrl = "/static/dist";  // dev
-const publicUrl  = "https://djfdm802jwooz.cloudfront.net/static/color_doodle/dist";
+const publicUrl  = "https://djfdm802jwooz.cloudfront.net/static/color_doodle_dist";
 
 module.exports = {
     entry: {
       bundle: [`${baseDir}App.js`, `${baseDir}/scss/doodle.scss`]
+    },
+    output: {
+        path: path.resolve(__dirname, "./color_doodle_dist")
     },
     module: {
         rules: [
