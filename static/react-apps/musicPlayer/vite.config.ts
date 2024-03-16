5482015d083323f8cb2ef6e861d16d68ae391378
Tyler <tylerkershner@gmail.com>
2024-03-16 14:15:10 -0400

    fullscreen sizing fix

--- static/react-apps/musicPlayer/vite.config.ts
+++ static/react-apps/musicPlayer/vite.config.ts
@@ -3,17 +3,12 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 const url = `${reactAppsUrl}musicPlayer/dist`
-const devMode = false
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: url,
   plugins: [react()],
   build: {
-    minify: devMode ? false : true,
-    terserOptions: {
-      mangle: devMode ? false : true,
-    },
     rollupOptions: {
       output: {
         entryFileNames: `kershPlayer.js`,
