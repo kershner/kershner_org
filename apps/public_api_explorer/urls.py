56ed798b3a6ed266b2263179ab41ffae3cd85cff
Tyler <tylerkershner@gmail.com>
2024-11-20 23:02:30 -0500

    fix url

--- apps/public_api_explorer/urls.py
+++ apps/public_api_explorer/urls.py
@@ -1,8 +1,7 @@
+from django.urls import path, re_path
 import apps.public_api_explorer.views as public_api_explorer_views
-from django.urls import path
-
 
 public_api_explorer_patterns = [
     path('', public_api_explorer_views.PublicApiExplorerHomeView.as_view(), name='public_api_explorer'),
-    path('view/', public_api_explorer_views.PublicApiExplorerHomeView.as_view(), name='public_api_explorer_view'),
+    re_path(r'^view/?$', public_api_explorer_views.PublicApiExplorerHomeView.as_view(), name='public_api_explorer_view'),
 ]
