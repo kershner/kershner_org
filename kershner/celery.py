c63ece6ccf5c0f63ed1a7415f05a3a9adfe34f74
Tyler <tylerkershner@gmail.com>
2025-05-18 18:44:49 -0400

    small tweak

--- kershner/celery.py
+++ kershner/celery.py
@@ -1,5 +1,4 @@
 from __future__ import absolute_import
-from celery.schedules import crontab
 from django.conf import settings
 from celery import Celery
 import os
@@ -10,19 +9,3 @@ os.environ.setdefault('FORKED_BY_MULTIPROCESSING', '1')
 app = Celery('kershner')
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-
-
-# # TODO - get celery-beat running (migrate to redis or postgres)
-# app.conf.beat_schedule = {
-#     'post-to-bluesky-morning': {
-#         'task': 'apps.daggerwalk.tasks.post_to_bluesky',
-#         'schedule': crontab(minute=20, hour=16),
-#     },
-#     # 'post-to-bluesky-evening': {
-#     #     'task': 'apps.daggerwalk.tasks.post_to_bluesky',
-#     #     'schedule': crontab(minute=30, hour=21),  # 9:30 PM Eastern
-#     # },
-# }
-
-# app.conf.timezone = 'UTC'
-# app.conf.enable_utc = True
