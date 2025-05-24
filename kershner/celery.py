067e5cfe93c14efe1b03c4997cf460407e9a303a
Tyler <tylerkershner@gmail.com>
2025-05-23 20:22:51 -0400

    tweak celery config

--- kershner/celery.py
+++ kershner/celery.py
@@ -4,19 +4,19 @@ from django.conf import settings
 from celery import Celery
 import os
 
-CELERY_BEAT_SCHEDULE = {
-    'daggerwalk-post-to-bluesky-morning': {
-        'task': 'apps.daggerwalk.tasks.post_to_bluesky',
-         'schedule': crontab(minute=10, hour=11),  # 11:10 AM Eastern (local time)
-    },
-    'daggerwalk-post-to-bluesky-evening': {
-        'task': 'apps.daggerwalk.tasks.post_to_bluesky',
-        'schedule': crontab(minute=20, hour=20),   # 8:20 PM Eastern (local time)
-    },
-}
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'site_config.settings.prod')
 os.environ.setdefault('FORKED_BY_MULTIPROCESSING', '1')
 app = Celery('kershner')
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+app.conf.beat_schedule = {
+    'daggerwalk-post-to-bluesky-morning': {
+        'task': 'apps.daggerwalk.tasks.post_to_bluesky',
+        'schedule': crontab(minute=10, hour=11),  # 11:10 AM Eastern
+    },
+    'daggerwalk-post-to-bluesky-evening': {
+        'task': 'apps.daggerwalk.tasks.post_to_bluesky',
+        'schedule': crontab(minute=27, hour=20),  # 8:27 PM Eastern
+    },
+}
