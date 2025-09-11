38fa57037c6fdd62efcbfe0f511aeffe379c5827
Tyler <tylerkershner@gmail.com>
2025-09-11 19:09:49 -0400

    testing new nginx and gunicorn confs

--- site_config/gunicorn/prod.py
+++ site_config/gunicorn/prod.py
@@ -4,16 +4,20 @@
 wsgi_app = "kershner.wsgi:application"
 
 # The number of worker processes for handling requests
-workers = 1
-max_requests = 100
-max_requests_jitter = 10
+workers = 3
+max_requests = 1000
+max_requests_jitter = 100
 
 # Allow concurrency without extra worker processes
 worker_class = "gthread"
-threads = 4
+threads = 8
+
+keepalive = 2
+timeout = 60
+graceful_timeout = 30
 
 # The socket to bind
-bind = "0.0.0.0:8000"
+bind = "127.0.0.1:8000"
 
 # Write access and error info to /var/log
 accesslog = "/var/log/gunicorn/access.log"
