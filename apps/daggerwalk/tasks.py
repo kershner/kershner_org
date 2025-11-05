96d0f9057c3c80cfdf30f86c5195e4e65d88067e
Tyler <tylerkershner@gmail.com>
2025-11-04 23:20:40 -0500

    tweak values for map screenshot

--- apps/daggerwalk/tasks.py
+++ apps/daggerwalk/tasks.py
@@ -466,7 +466,7 @@ def post_screenshot_reply_to_video(client: Client, uri: str, cid: str, log_data)
                 executable_path=settings.PLAYWRIGHT_CHROMIUM_PATH
             )
             page = browser.new_page(
-                viewport={"width": 1920, "height": 1080},
+                viewport={"width": 1400, "height": 710},
                 device_scale_factor=2
             )
 
@@ -483,7 +483,7 @@ def post_screenshot_reply_to_video(client: Client, uri: str, cid: str, log_data)
             quest_element.screenshot(path=screenshots["quest"])
             logger.info(f"Screenshot saved: {screenshots['quest']}")
 
-            prepare_leaflet_map(page, x=525, y=250, zoom_level=1.123, delay=1.5)
+            prepare_leaflet_map(page, x=500, y=250, zoom_level=0.6, delay=1.5)
 
             logger.info("Taking map screenshot...")
             page.locator("#map").screenshot(path=screenshots["world"])
