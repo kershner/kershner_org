b35649afab158bee3207aff81fa13729fe2aff2c
Tyler <tylerkershner@gmail.com>
2026-01-15 23:25:21 -0500

    tweaks

--- apps/pi_stuff/admin.py
+++ apps/pi_stuff/admin.py
@@ -2,11 +2,20 @@ from django.contrib import admin
 from .models import Category, Playlist
 
 
+class PlaylistInline(admin.TabularInline):
+    model = Playlist
+    extra = 0
+    fields = ("name", "youtube_playlist_id")
+    ordering = ("name",)
+    show_change_link = True
+
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name']
     ordering = ['name']
     search_fields = ['name']
+    inlines = (PlaylistInline,)
 
 
 @admin.register(Playlist)
