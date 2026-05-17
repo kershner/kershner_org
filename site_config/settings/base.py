e902f64f60dbc89bcb9a791d6ba758ed72946ad0
Tyler <tylerkershner@gmail.com>
2026-05-16 21:12:22 -0400

    move subdomain middleware above commonmiddleware

--- site_config/settings/base.py
+++ site_config/settings/base.py
@@ -43,12 +43,12 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'kershner.middleware.SubdomainMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'kershner.middleware.SubdomainMiddleware',
 ]
 
 ROOT_URLCONF = 'kershner.urls'
